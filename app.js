f6d9af9b524be1bc96e077f2e6e32a555cb812b5
bratanac24 <acar998@gmail.com>
2021-10-07 17:04:32 +0200

    finished design

---
+++ app.js
@@ -0,0 +1,8 @@
+let darkMode = () =>{
+    mode = document.getElementById("body");
+    mode.classList.add("dark-mode");
+}
+let lightMode = () =>{
+    mode = document.getElementById("body");
+    mode.classList.remove("dark-mode");
+}
